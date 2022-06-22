f29daae5864da38b6fc5dbac7433b65fc87e2d6c
Brian <brianzhang234@gmail.com>
2022-06-22 16:00:44 -0500

    Declare functions

---
+++ script.js
@@ -0,0 +1,15 @@
+
+//Randomly returns either "Rock", "Paper", or "Scissors"
+function computerPlay() {
+    
+}
+
+//Takes two parameters and returns the winner
+function playRound(playerSelection, computerSelection) {
+
+}
+
+//Plays 5 rounds and returns the winner
+function game() {
+
+}
